34fc1b36f4fd7ad6af6ef374c1f507bce904e643
david-cornejo <davidcg914@gmail.com>
2024-03-19 01:49:36 -0600

    Aplicacion funcional

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,6 +1,15 @@
+/*
+Práctica 1: Aplicacion de almacenamiento remoto de archivos
+Creado por: Cornejo García David y Flores Melo Alan Nicolás
+Código: Cliente V1.0
+*/
+
 import java.io.*;
 import java.net.*;
 import java.nio.file.*;
+import java.util.stream.Stream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Cliente {
     private Socket commandSocket;
@@ -12,14 +21,16 @@ public class Cliente {
     private final int serverPort;
     private final int dataPort;
 
+    // Constructor de la clase Cliente con los parámetros de dirección IP, puerto de comandos y puerto de datos
     public Cliente(String ip, int port, int dataPort) {
         this.serverAddress = ip;
         this.serverPort = port;
         this.dataPort = dataPort;
     }
 
+    // Método para iniciar la conexión con el servidor y crear los flujos de entrada y salida
     public void startConnection() {
-        try {
+        try { // Intenta crear el socket de comandos y los flujos de entrada y salida
             commandSocket = new Socket(serverAddress, serverPort);
             out = new PrintWriter(commandSocket.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(commandSocket.getInputStream()));
@@ -29,55 +40,79 @@ public class Cliente {
         }
     }
 
+    // Método para enviar comandos al servidor a través del flujo de salida del socket de comandos
     public void sendCommand(String command) {
         out.println(command);
     }
 
+    // Método para enviar archivos al servidor a través del socket de datos
     private void sendData(File file) throws IOException {
         OutputStream outStream = dataSocket.getOutputStream();
-        Files.copy(file.toPath(), outStream);
+        Files.copy(file.toPath(), outStream); // Copia el archivo al flujo de salida del socket
         outStream.close();
     }
 
+    // Método para recibir archivos o directorios del servidor a través del socket de datos
     private void receiveData(String fileName) throws IOException {
-        InputStream inStream = dataSocket.getInputStream();
-        Files.copy(inStream, currentDirectory.resolve(fileName), StandardCopyOption.REPLACE_EXISTING);
-        inStream.close();
+        Path filePath = currentDirectory.resolve(fileName);
+        try (InputStream inStream = dataSocket.getInputStream()) { // Crea un flujo de entrada para recibir el archivo
+            Files.copy(inStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+        }
+        if (fileName.endsWith(".zip")) {
+            System.out.println("Carpeta recibida desde el servidor.");
+        } else {
+            System.out.println("Archivo recibido desde el servidor.");;
+        }
     }
 
+    // Método para manejar los comandos ingresados por el usuario
     public void handleCommand(String command) {
         try {
-            if (command.startsWith("list")) {
+            if (command.startsWith("list")) { // Si el comando es "list", se envía al servidor y se imprime la lista de archivos
                 sendCommand(command);
                 String line;
                 while (!(line = in.readLine()).equals("END-OF-LIST")) {
                     System.out.println(line);
                 }
-            } else if (command.startsWith("put")) {
-                String[] parts = command.split(" ", 2);
+            } else if (command.startsWith("put")) { // Si el comando es "put", se envía el archivo al servidor a través del socket de datos
+                String[] parts = command.split(" ", 2); // Divide el comando en dos partes, el comando y el nombre del archivo
                 if (parts.length > 1) {
-                    sendCommand(command);
                     dataSocket = new Socket(serverAddress, dataPort);
-                    File file = currentDirectory.resolve(parts[1]).toFile();
+                    File file = currentDirectory.resolve(parts[1]).toFile(); // Obtiene el archivo a partir del nombre ingresado
                     if (file.exists()) {
-                        sendData(file);
-                        System.out.println("Archivo enviado al servidor.");
-                    } else {
+                        if (file.isDirectory()) { // Si el archivo es un directorio, se comprime y se envía al servidor
+                            String zipFileName = file.getName() + ".zip";
+                            Path zipPath = currentDirectory.resolve(zipFileName);
+                            compressDirectory(file.toPath(), zipPath);
+
+                            // Envía el comando con el nombre del archivo .zip
+                            sendCommand("put " + zipFileName);
+
+                            sendData(zipPath.toFile());
+                            System.out.println("Directorio comprimido y enviado al servidor.");
+                            Files.delete(zipPath); // Borra el archivo .zip temporal
+
+                        } else { // Si el archivo es un archivo regular, se envía al servidor
+                            sendCommand(command);
+                            sendData(file);
+                            System.out.println("Archivo enviado al servidor.");;
+                        }
                         dataSocket.close();
+                    } else {
                         System.out.println("Archivo no encontrado.");
                     }
-                    dataSocket.close();
                 }
-            } else if (command.startsWith("get")) {
+            } else if (command.startsWith("get")) { // Si el comando es "get", se recibe el archivo del servidor a través del socket de datos
                 String[] parts = command.split(" ", 2);
                 if (parts.length > 1) {
                     sendCommand(command);
+                    String fileInfo = in.readLine();
                     dataSocket = new Socket(serverAddress, dataPort);
-                    receiveData(parts[1]);
-                    System.out.println("Archivo recibido del servidor.");
+                    receiveData(fileInfo);
                     dataSocket.close();
+
                 }
-            } else {
+            } else { // Si el comando no es "list", "put" o "get", se envía al servidor y se imprime la respuesta
                 sendCommand(command);
                 System.out.println(in.readLine());
             }
@@ -86,8 +121,27 @@ public class Cliente {
         }
     }
 
+    // Método de compresión de directorio a archivo .zip
+    private void compressDirectory(Path directoryToZip, Path zipPath) throws IOException {
+        try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipPath.toFile()));
+             Stream<Path> paths = Files.walk(directoryToZip)) {
+            paths.filter(path -> !Files.isDirectory(path))
+                    .forEach(path -> {
+                        ZipEntry zipEntry = new ZipEntry(directoryToZip.relativize(path).toString());
+                        try {
+                            zos.putNextEntry(zipEntry);
+                            Files.copy(path, zos);
+                            zos.closeEntry();
+                        } catch (IOException e) {
+                            System.err.println("Error while zipping: " + e.getMessage());
+                        }
+                    });
+        }
+    }
+
+    // Método para detener la conexión con el servidor
     public void stopConnection() {
-        try {
+        try { // Intenta cerrar los flujos de entrada y salida y el socket de comandos
             if (in != null) in.close();
             if (out != null) out.close();
             if (commandSocket != null) commandSocket.close();
@@ -97,6 +151,7 @@ public class Cliente {
         }
     }
 
+    // Método principal de la clase Cliente en donde se crea un objeto Cliente y se manejan los comandos ingresados por el usuario
     public static void main(String[] args) {
         Cliente client = new Cliente("127.0.0.1", 5555, 5556);
         client.startConnection();
--- src/Servidor.java
+++ src/Servidor.java
@@ -1,7 +1,15 @@
+/*
+Práctica 1: Aplicacion de almacenamiento remoto de archivos
+Creado por: Cornejo García David y Flores Melo Alan Nicolás
+Código: Servidor V1.0
+*/
+
 import java.io.*;
 import java.net.*;
 import java.nio.file.*;
 import java.util.stream.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Servidor {
     private ServerSocket serverSocket;
@@ -12,6 +20,7 @@ public class Servidor {
     private ServerSocket dataServerSocket;
     private Socket dataSocket;
 
+    //Se crean los sockets del servidor en el puerto 5555 para comandos y 5556 para datos
     public Servidor(int port, int dataPort) {
         try {
             serverSocket = new ServerSocket(port);
@@ -21,17 +30,17 @@ public class Servidor {
         }
     }
 
+    //metodo para iniciar el servidor y esperar a que un cliente se conecte
     public void start() {
         System.out.println("Servidor iniciado en el puerto " + serverSocket.getLocalPort() + ". Esperando clientes...");
         try {
             clientSocket = serverSocket.accept();
-            System.out.println("Cliente conectado: " + clientSocket.getInetAddress());
-            out = new PrintWriter(clientSocket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
+            System.out.println("Cliente conectado: " + clientSocket.getInetAddress());  // Muestra la dirección IP del cliente conectado
+            out = new PrintWriter(clientSocket.getOutputStream(), true); // Se crea el flujo de salida para enviar mensajes al cliente
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); // Se crea el flujo de entrada para recibir mensajes del cliente
             String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                if ("quit".equals(inputLine)) {
+            while ((inputLine = in.readLine()) != null) { // Espera a que el cliente envíe un mensaje
+                if ("quit".equals(inputLine)) { // Si el cliente envía "quit", se termina la sesión
                     out.println("Terminando sesión...");
                     break;
                 }
@@ -43,27 +52,29 @@ public class Servidor {
             stop();
         }
     }
+
+    //procesamiento de los comandos que el cliente envia al servidor
     private void processCommand(String command) {
         String[] parts = command.split(" ", 2);
         String response = "";
         try {
             switch (parts[0]) {
-                case "list":
+                case "list": // Lista los archivos y directorios del directorio actual
                     response = listDirectory(currentDirectory);
                     break;
-                case "mkdir":
-                    response = createDirectory(parts.length > 1 ? parts[1] : "");
+                case "mkdir": // Crea un directorio
+                    response = createDirectory(parts.length > 1 ? parts[1] : ""); // Si el comando tiene más de una palabra, la segunda palabra es el nombre del directorio
                     break;
-                case "rmdir":
+                case "rmdir": // Elimina un directorio o archivo
                     response = removeDirectoryOrFile(parts.length > 1 ? parts[1] : "");
                     break;
-                case "cd":
+                case "cd": // Cambia de directorio
                     response = changeDirectory(parts.length > 1 ? parts[1] : "");
                     break;
-                case "put":
+                case "put": // Recibe un archivo el servidor
                     acceptFile(parts.length > 1 ? parts[1] : "");
                     break;
-                case "get":
+                case "get": // Envía un archivo al cliente
                     sendFile(parts.length > 1 ? parts[1] : "");
                     break;
                 default:
@@ -76,15 +87,17 @@ public class Servidor {
         out.println(response); // Envía la respuesta final al cliente
     }
 
+    //metodo para listar los archivos y directorios del directorio actual
     private String listDirectory(Path directory) throws IOException {
-        StringBuilder response = new StringBuilder();
-        try (Stream<Path> paths = Files.list(directory)) {
+        StringBuilder response = new StringBuilder(); // Se crea un StringBuilder para concatenar los nombres de los archivos y directorios
+        try (Stream<Path> paths = Files.list(directory)) { // Se obtiene un Stream de los archivos y directorios del directorio actual
             paths.map(path -> path.getFileName().toString())
                     .forEach(filename -> response.append(filename).append("\n"));
         }
         response.append("END-OF-LIST");
         return response.toString();
     }
+
 
     private String createDirectory(String dirName) {
         if (dirName.isEmpty()) {
@@ -99,21 +112,23 @@ public class Servidor {
         }
     }
 
+    //metodo para eliminar un directorio o archivo
     private String removeDirectoryOrFile(String name) {
         if (name.isEmpty()) {
             return "Nombre de archivo o directorio no especificado.";
         }
-        Path path = currentDirectory.resolve(name);
+        Path path = currentDirectory.resolve(name); // Se obtiene el Path del archivo o directorio a eliminar
         try {
-            Files.deleteIfExists(path);
+            Files.deleteIfExists(path); // Se intenta eliminar el archivo o directorio
             return "Eliminado: " + path;
         } catch (IOException e) {
             return "Error al eliminar: " + e.getMessage();
         }
     }
 
+    //metodo para cambiar de directorio
     private String changeDirectory(String dirName) {
-        if (dirName.isEmpty() || "..".equals(dirName)) {
+        if (dirName.isEmpty() || "..".equals(dirName)) { // Si el nombre del directorio está vacío o es "..", se sube un nivel
             currentDirectory = currentDirectory.getParent();
             if (currentDirectory == null) {
                 currentDirectory = Paths.get(System.getProperty("user.dir"));
@@ -122,7 +137,7 @@ public class Servidor {
         }
 
         Path newPath = currentDirectory.resolve(dirName);
-        if (Files.exists(newPath) && Files.isDirectory(newPath)) {
+        if (Files.exists(newPath) && Files.isDirectory(newPath)) { // Si el directorio existe y es un directorio, se cambia
             currentDirectory = newPath;
             return "Directorio cambiado a " + newPath;
         } else {
@@ -130,11 +145,12 @@ public class Servidor {
         }
     }
 
+    //metodo para recibir un archivo del cliente
     private void acceptFile(String fileName) {
-        try {
+        try { // Se crea un nuevo socket para la conexión de datos
             dataSocket = dataServerSocket.accept();
-            InputStream dataIn = new BufferedInputStream(dataSocket.getInputStream());
-            Files.copy(dataIn, currentDirectory.resolve(fileName), StandardCopyOption.REPLACE_EXISTING);
+            InputStream dataIn = new BufferedInputStream(dataSocket.getInputStream()); // Se crea un flujo de entrada para recibir el archivo
+            Files.copy(dataIn, currentDirectory.resolve(fileName), StandardCopyOption.REPLACE_EXISTING); // Se copia el archivo al directorio actual
             dataIn.close();
             System.out.println("Archivo " + fileName + " recibido.");
         } catch (IOException e) {
@@ -150,16 +166,33 @@ public class Servidor {
         }
     }
 
-    private void sendFile(String fileName) {
-        Path filePath = currentDirectory.resolve(fileName);
-        if (!Files.exists(filePath)) {
+    //metodo para enviar un archivo al cliente
+    private void sendFile(String fileName) throws IOException {
+        Path fileToSend = currentDirectory.resolve(fileName); // Se obtiene el Path del archivo a enviar
+
+        boolean isDirectory = Files.isDirectory(fileToSend); // Se verifica si el archivo es un directorio
+
+        if (!Files.exists(fileToSend)) {
             out.println("Archivo no encontrado.");
-            return;
         }
+
+        if (isDirectory) { // Si el archivo es un directorio, se comprime
+            fileName = fileName + ".zip";
+            fileToSend = zipDirectory(fileToSend, fileName);
+        }
+
+        // Envía metadatos sobre el archivo a enviar para identificarlo en el cliente
+        out.println(fileName);
+
+        // Procede a enviar el archivo
         try {
-            dataSocket = dataServerSocket.accept();
-            OutputStream dataOut = new BufferedOutputStream(dataSocket.getOutputStream());
-            Files.copy(filePath, dataOut);
+            dataSocket = dataServerSocket.accept(); // Se crea un nuevo socket para la conexión de datos
+            OutputStream dataOut = new BufferedOutputStream(dataSocket.getOutputStream()); // Se crea un flujo de salida para enviar el archivo
+            Files.copy(fileToSend, dataOut);
+            if(fileName.endsWith(".zip")) { // Si el archivo es un directorio comprimido, se borra el archivo .zip temporal
+                Files.delete(fileToSend);
+            }
+            dataOut.flush();
             dataOut.close();
             System.out.println("Archivo " + fileName + " enviado.");
         } catch (IOException e) {
@@ -175,8 +208,29 @@ public class Servidor {
         }
     }
 
+    //metodo para comprimir un directorio
+    private Path zipDirectory(Path directory, String zipFileName) throws IOException {
+        Path zipFilePath = currentDirectory.resolve(zipFileName);
+        try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipFilePath.toFile()));
+             Stream<Path> paths = Files.walk(directory)) { // Se obtiene un Stream de los archivos y directorios del directorio a comprimir
+            paths.filter(path -> !Files.isDirectory(path)) // Se filtran los archivos
+                    .forEach(path -> { // Se crea un ZipEntry para cada archivo y se copia al archivo .zip
+                        ZipEntry zipEntry = new ZipEntry(directory.relativize(path).toString());
+                        try {
+                            zos.putNextEntry(zipEntry);
+                            Files.copy(path, zos);
+                            zos.closeEntry();
+                        } catch (IOException e) {
+                            System.err.println("Error while zipping: " + e.getMessage());
+                        }
+                    });
+        }
+        return zipFilePath; // Se devuelve el Path del archivo .zip
+    }
+
+    //metodo para detener el servidor
     public void stop() {
-        try {
+        try { // Se cierran los flujos y sockets
             if (in != null) in.close();
             if (out != null) out.close();
             if (clientSocket != null && !clientSocket.isClosed()) clientSocket.close();
@@ -187,6 +241,7 @@ public class Servidor {
         }
     }
 
+    //metodo main para iniciar el servidor donde indicamos los puertos para comandos y datos
     public static void main(String[] args) {
         Servidor server = new Servidor(5555, 5556);
         server.start();
